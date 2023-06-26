99a6aa73e5dd28e1ad43ca87dd403a1330ccb4ea
Adrian Insaurralde Avalos <adrianinsaval@gmail.com>
2023-06-26 05:56:45 -0400

    try specifying arm64 target for macos cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -42,15 +42,16 @@ if [[ ${HOST} =~ .*darwin.* ]]; then
     curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-6-6_360DF97D-ED08-4ccf-A55E-0BF905E58476/3DxWareMac_v10-6-6_r3234.dmg'
   else
     curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-7-0_B564CC6A-6E81-42b0-82EC-418EA823B81A/3DxWareMac_v10-7-0_r3411.dmg'
+    if [[ ${build_platform} != ${target_platform} ]]; then
+      CXXFLAGS="${CXXFLAGS} --target=arm64-apple-darwin"
+    fi
   fi
   hdiutil attach -readonly /tmp/3dFW.dmg
   sudo installer -package /Volumes/3Dconnexion\ Software/Install\ 3Dconnexion\ software.pkg -target /
   diskutil eject /Volumes/3Dconnexion\ Software
   CMAKE_PLATFORM_FLAGS+=(-DFREECAD_USE_3DCONNEXION:BOOL=ON)
   CMAKE_PLATFORM_FLAGS+=(-D3DCONNEXIONCLIENT_FRAMEWORK:FILEPATH="/Library/Frameworks/3DconnexionClient.framework")
-fi
 
-if [[ ${HOST} =~ .*darwin.* ]]; then
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
